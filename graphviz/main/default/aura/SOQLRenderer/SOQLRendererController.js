c6ecdf7dc29ac1b3f02fdf1e6d41b9ee16b262ae
Steve Buikhuizen <steve.buikhuizen@gmail.com>
2018-06-03 17:28:38 +1000

    FEATURE SOQL renderer is visible below diagram, with copy buttons and other controls

--- graphviz/main/default/aura/SOQLRenderer/SOQLRendererController.js
+++ graphviz/main/default/aura/SOQLRenderer/SOQLRendererController.js
@@ -4,14 +4,13 @@
     },
     fromChange: function (component, event, helper) {
         if ($A.util.isEmpty(component.get("v.from"))) {
-
+            component.set("v.prompt", "Choose a SOQL FROM object:")
         } else {
-            try {
-                var rendered = window.pure.soql.diagramAsSelects(component.get("v.diagram"), event.getParam("value"));
-                helper.updateSOQL(component, rendered.selectLists);
-            } catch (e) {
-                console.log(e);
-            }
+            component.set("v.prompt", "FROM")
+            var rendered = window.pure.soql.diagramAsSelects(component.get("v.diagram"), event.getParam("value"));
+            var soql = window.pure.soql.diagramSelectsAsSOQL(rendered.selectLists, event.getParam("value"), false);
+            component.set("v.soql", soql);
+            component.set("v.selectLists", rendered.selectLists);
         }
     },
     diagramChange: function (component, event, helper) {
@@ -22,7 +21,24 @@
         } else {
             var rendered = window.pure.soql.diagramAsSelects(event.getParam("value"), component.get("v.from"));
             helper.updateEntities(component, rendered.entities);
-            helper.updateSOQL(component, rendered.selectLists);
+            var soql = window.pure.soql.diagramSelectsAsSOQL(rendered.selectLists, event.getParam("value"), false);
+            component.set("v.soql", soql);
+            component.set("v.selectLists", rendered.selectLists);
         }
+    },
+    handleClearClick: function (component, event, helper) {
+        component.set("v.from", undefined);
+    },
+    handleCopyClick: function (component, event, helper) {
+        var success = Core.AuraUtils.copyToClipboard(component.get("v.soql"));
+        if (!success) {
+            window.alert("Copy to clipboard failed. Try another browser?");
+        }
+    },
+    handleSOQLClick: function (component, event, helper) {
+        component.set("v.mode", "soql");
+    },
+    handlePreviewClick: function (component, event, helper) {
+        component.set("v.mode", "preview");
     }
 })
--- graphviz/main/default/aura/SOQLRenderer/SOQLRendererHelper.js
+++ graphviz/main/default/aura/SOQLRenderer/SOQLRendererHelper.js
@@ -1,21 +1,4 @@
 ({
-    updateSOQL: function (component, selectLists) {
-        var soql = "SELECT ";
-        selectLists.forEach(function (list) {
-            soql += list;
-            soql += ",";
-            soql += "\n";
-        });
-
-        // trim the comma from the last select list
-        soql = soql.substr(0, soql.length - 2);
-        soql += "\n";
-
-        soql += "FROM ";
-        soql += component.get("v.from");
-
-        component.set("v.soql", soql);
-    },
     updateEntities: function (component, entities) {
         var opts = [];
         entities.forEach(function (entity) {
