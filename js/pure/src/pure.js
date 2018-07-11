4ce9c3762a0be21b968489de1b0271a3d66f7a7f
Steve Buikhuizen <steve.buikhuizen@gmail.com>
2018-07-11 22:40:22 +1000

    FIX including JSON schema validator in pure artifact

--- js/pure/src/pure.js
+++ js/pure/src/pure.js
@@ -1,7 +1,9 @@
 var soql = require('./soql.js');
 var graphviz = require('./graphviz.js');
+var Validator = require('jsonschema').Validator;
 
 module.exports = {
     soql: soql,
-    graphviz: graphviz
+    graphviz: graphviz,
+    validator: Validator
 };
