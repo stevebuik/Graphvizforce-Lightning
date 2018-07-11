6d678cffdc0a28613e947346a5c491712cf7ad98
Steve Buikhuizen <steve.buikhuizen@gmail.com>
2018-07-11 22:46:27 +1000

    FIX adding schemas.js to pure artifact

--- js/pure/src/pure.js
+++ js/pure/src/pure.js
@@ -1,6 +1,7 @@
 var soql = require('./soql.js');
 var graphviz = require('./graphviz.js');
 var Validator = require('jsonschema').Validator;
+var schemas = require('../src/schemas.js');
 
 module.exports = {
     soql: soql,
