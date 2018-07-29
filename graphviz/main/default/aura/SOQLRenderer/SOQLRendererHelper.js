faadfecaae7789c57f6b60a3c4b6190a6074d09d
Steve Buikhuizen <steve.buikhuizen@gmail.com>
2018-07-29 21:18:29 +1000

    FIX SOQL from losing its value when diagram changes
    was a race condition, found solution in docs

--- graphviz/main/default/aura/SOQLRenderer/SOQLRendererHelper.js
+++ graphviz/main/default/aura/SOQLRenderer/SOQLRendererHelper.js
@@ -1,8 +1,13 @@
 ({
     updateEntities: function (component, entities) {
         var opts = [];
+        var from = component.get("v.from");
         entities.forEach(function (entity) {
-            opts.push({label: entity, value: entity});
+            opts.push({
+                label: entity,
+                value: entity,
+                selected: entity == from // required to help select dom stay correct when options change. see lightning:select docs race condition
+            });
         });
         component.set("v.entityOptions", opts);
     }
