68fd30f30215f0df54baaf682a3e7e434cc53f79
ZHIREN GUAN <jasong327@gmail.com>
2018-08-08 12:54:15 +1000

    CHORE Cleaned up deprecated code

--- graphviz/main/default/aura/DiagramViewer/DiagramViewerController.js
+++ graphviz/main/default/aura/DiagramViewer/DiagramViewerController.js
@@ -13,11 +13,10 @@
         var graphvizContent = component.get('v.graphvizContent');
         var initialised = component.get('v.initialised');
         if(initialised && !$A.util.isEmpty(graphvizContent)){
-            /*
-            var erdMarkup = GraphvizForce.DiagramHelper.renderSVGMarkup(graphvizContent, 'svg');
-            document.getElementById("graph").innerHTML = erdMarkup;
-            */
+            // Clear diagram svg (Required step)
             document.getElementById("graph").innerHTML = "";
+
+            // Generate markup
             var erdMarkup = d3.select("#graph")
                                 .graphviz()
                                 .fade(false)
@@ -28,6 +27,7 @@
                                         .attr("height", "70vh");
                                 });
 
+            // Set markup
             component.set('v.graphviz', erdMarkup);
             component.getEvent('onDiagramRendered').setParams({scope:erdMarkup}).fire();
         }
